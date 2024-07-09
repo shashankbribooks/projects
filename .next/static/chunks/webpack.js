a6c48a7c69f2d9a12212b7ac6cc601c94290fe38
Imtiyaz-BriBooks <imtiyaz@bribooks.com>
2024-07-09 12:15:44 +0530

    minor bugs fix inter-generational

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -57,11 +57,6 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	!function() {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	}();
-/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -136,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "31260cd27f3eaea9"; }
+/******/ 		__webpack_require__.h = function() { return "6f8e6b89b2f1a7cd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
